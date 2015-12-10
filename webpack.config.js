8e5fef50a4573f061f9d35245c1ef3d6b72afe43
Dale Karp <dale@dale.io>
2015-12-10 03:09:34 -0500

    [CHANGE] Updated webpack config with variables, html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var ENTRY_PATH = path.resolve(ROOT_PATH, 'src/js/main.js');
+var SRC_PATH = path.resolve(ROOT_PATH, 'src');
+var JS_PATH = path.resolve(ROOT_PATH, 'src/js');
+var SHADER_PATH = path.resolve(ROOT_PATH, 'src/shaders');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+
 module.exports = {
-    entry: "./src/js/main.js",
+    entry: ENTRY_PATH,
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
     output: {
-        path: __dirname + '/dist',
-        filename: "bundle.js"
+        path: BUILD_PATH,
+        filename: 'bundle.js'
     },
     resolve: {
-        root: [__dirname + '/src/js', __dirname + '/src']
+        root: [JS_PATH, SRC_PATH]
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
+                include: JS_PATH,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
@@ -20,6 +35,7 @@ module.exports = {
             },
             {
                 test: /\.glsl$/,
+                include: SHADER_PATH,
                 loader: 'webpack-glsl'
             }
         ]
